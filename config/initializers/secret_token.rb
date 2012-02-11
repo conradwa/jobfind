2c8b78eae6f447cc4da17d489a6397c473b8d29d
Conrad Wadowski <conrad.wadowski@gmail.com>
2012-02-10 22:44:01 -0500

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobfind::Application.config.secret_token = '25ac66df0f0139ca51fc355d11a74441ed278b19a72d4ca6569bcfc8682d540650e70affd997417de18ba1f93e1cd4a27a081870630d50dda3ebc99b74eeda4b'
